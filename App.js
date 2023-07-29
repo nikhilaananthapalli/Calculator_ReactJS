452683b3c0dda9a25d78d91b99702e7292c2ec24
nikhilaananthapalli <140099221+nikhilaananthapalli@users.noreply.github.com>
2023-07-29 15:50:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Components/Calci.css'
+import './Components/Calci'
+import Calci from './Components/Calci';
+
+function App() {
+  return (
+    <div className="App">
+      <Calci/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Calci.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+function Calci(){
+    const [result,setResult] = useState("");
+    const handleButton = (e) =>{
+        setResult(result.concat(e.target.name));
+    }
+    const clear =()=>{
+        setResult("");
+    }
+    const backspace= () =>{
+        setResult(result.slice(0,-1));
+    }
+    const evaluate = ()=>{
+        try{
+        setResult(eval(result).toString());
+        }
+        catch(err){
+            setResult("Error");
+        }
+    }
+
+    return (
+       <div className='Main'>
+        <div className='Container'>
+            <form>
+                <input type='text' value={result}></input>
+            </form>
+        </div>
+        <div className='Keypad'>
+            <button onClick={clear} id='clear'>Clear</button>
+            <button onClick={backspace} id='c'>C</button>
+            <button onClick={handleButton} name='/'>/</button>
+            <button onClick={handleButton} name='7'>7</button>
+            <button onClick={handleButton} name='8'>8</button>
+            <button onClick={handleButton} name='9'>9</button>
+            <button onClick={handleButton} name='%'>%</button>
+            <button onClick={handleButton} name='6'>6</button>
+            <button onClick={handleButton} name='5'>5</button>
+            <button onClick={handleButton} name='4'>4</button>
+            <button onClick={handleButton} name='+'>+</button>
+            <button onClick={handleButton} name='3'>3</button>
+            <button onClick={handleButton} name='2'>2</button>
+            <button onClick={handleButton} name='1'>1</button>
+            <button onClick={handleButton} name='*'>*</button>
+            <button onClick={handleButton} name='0'>0</button>
+            <button onClick={handleButton} name='-'>-</button>
+            <button onClick={evaluate} id='result'>=</button>
+
+        </div>
+       </div>  
+    )
+    }
+export default Calci
